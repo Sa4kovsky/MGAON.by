d932f10948df71db163b929c6e28902774a41a79
Evgeny Sachkovsky <sa4kovsky@gmail.com>
2020-02-14 15:16:52 +0300

    THE END Localizer

--- Website/wwwroot/js/main.js
+++ Website/wwwroot/js/main.js
@@ -132,26 +132,7 @@ var x = 0;
 var profile = document.getElementById('profile');
 var div = document.createElement('div');
 
-function Add() {
-    div.id = 'input' + ++x;
-    div.innerHTML = '<input type="file" name="upload" id="uploadFile" /><br /> <button type="button" formnovalidate value="SendEmail" id="buttonSendEmailas">Отправить обращение</button>';
-    profile.appendChild(div);
-    $(document).ready(function () {
-        $('#buttonSendEmailas').click(function () {
-            var formData = new FormData();
-            formData.append('File', $('#uploadFile')[0].files[0]); // myFile is the input type="file" control
-            $.ajax({
-                url: '/service/uploadFile',
-                type: 'POST',
-                data: formData,
-                processData: false,
-                contentType: false
-            });
-        });
-    });
-}
-
-//Метод рисования новых форм для физ.лиц.
+//Метод рисования новых форм для физ.лиц (RU).
 function addInputNaturalPerson() {
     if (x < 1) {
         delInput();
@@ -193,12 +174,98 @@ function addInputNaturalPerson() {
     }
 }
 
-//Метод рисования новых форм для юр.лиц.
+//Метод рисования новых форм для юр.лиц (RU).
 function addInputLegalPerson() {
     if (x < 1) {
         delInput();
         div.id = 'input' + ++x;
         div.innerHTML = '<div class="form-group"><input type="text" class="form-control" name="designation" id="designation" placeholder="Наименование и (или) адрес организации либо должность лица*" required /></div> <div class="form-group"><input type="text" class="form-control" name="nameLegal" id="nameLegal" placeholder="Полное наименование юридического лица*" required /></div> <div class="form-group"><input type="text" class="form-control" name="fullName" id="fullName" placeholder="ФИО либо инициалы руководителя или лица, уполномоченного в установленном порядке подписывать обращения*" required /></div> <div class="form-group"><input type="text" class="form-control" name="address" id="address" placeholder="Местонахождение юридического лица*" /></div> <div class="form-group"><input type="email" class="form-control" name="email" id="email" placeholder="Электронная почта*" /></div>  <div class="form-group"><input type="text" class="form-control" name="tel" id="tel" placeholder="Контактный телефон" /></div> <div class="form-group"><textarea class="form-control" name="message" id="message" placeholder="Обращение*" maxlength="2000"></textarea></div> <div><p>Допустимыми форматами прикрепляемых документов и (или) сведений в электронном виде являются <em>Portable Document Format/A</em> (<em>PDF/A</em>), <em>Office Open XML</em> (<em>DOCX</em>), двойной формат с разметкой (<em>DOC</em>), Rich Text Format (<em>RTF</em>), текстовый файл (<em>TXT</em>), <em>Open Document Format</em> (<em>ODT</em>), формат архивации и сжатия данных (<em>ZIP, RAR</em>), <em>Portable Network Graphics</em> (<em>PNG</em>), <em>Tagged Image File Format</em> (<em>TIFF</em>), <em>Joint Photograph Experts Group</em> (<em>JPEG</em>), <em>Joint Photograph Group</em> (<em>JPG</em>).</p></div> <div class="form-group"><input type="file" name="upload" id="uploadFile" /></div> <div class="text-center" style="padding:20px"><button type="button" formnovalidate value="SendEmail" id="buttonSendEmail">Отправить обращение</button></div>';
+        profile.appendChild(div);
+        maskTel();
+        //ajax сробатывает при отправе сообщения, чтобы частично обновлять представление
+        $(document).ready(function () {
+            $('#buttonSendEmail').click(function () {
+                ValidName();
+                ValidNameLegal();
+                ValidMessage();
+                ValidDesignation();
+                ValidAddress();
+                ValidEmail();
+                if (_validName == true && _validMessage == true && _validDesignation == true && _validAddress == true && _validEmail == true && _validNameLegal == true) {
+                    var formData = new FormData();
+                    formData.append('File', $('#uploadFile')[0].files[0]); // myFile is the input type="file" control
+                    formData.append('Name', $('#fullName').val());
+                    formData.append('Address', $('#address').val());
+                    formData.append('Email', $('#email').val());
+                    formData.append('Phone', $('#tel').val());
+                    formData.append('Message', $('#message').val());
+                    formData.append('Designation', $('#designation').val());
+                    formData.append('NameLegal', $('#nameLegal').val());
+                    $.ajax({
+                        url: '/service/sendEmail',
+                        type: 'POST',
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        success: function (result) {
+                            $('#resultSendEmail').html(result);
+                        }
+                    });
+                    $('#exampleModalCenter').modal('show')
+                }
+            });
+        });
+    }
+}
+
+//Метод рисования новых форм для физ.лиц (BE).
+function addInputNaturalPersonBE() {
+    if (x < 1) {
+        delInput();
+        div.id = 'input' + ++x;
+        div.innerHTML = '<div class="form-group"><input type="text" class="form-control" name="designation" id="designation" placeholder="Назву і (або) адрас арганізацыі альбо пасаду асобы*" required /></div> <div class="form-group"><input type="text" class="form-control" name="fullName" id="fullName" placeholder="Прозвішча, імя*" required /></div> <div class="form-group"><input type="text" class="form-control" name="address" id="address" placeholder="Адрас месца жыхарства (месца знаходжання)*" /></div> <div class="form-group"><input type="email" class="form-control" name="email" id="email" placeholder="Электронная пошта*" /></div>  <div class="form-group"><input type="text" class="form-control" name="tel" id="tel" placeholder="Кантактны тэлефон" /></div> <div class="form-group"><textarea class="form-control" name="message" id="message" placeholder="Зварот*" maxlength="2000"></textarea></div> <div><p>Дапушчальнымі фарматамі дакументаў і (або) звестак, якія прымацоўваюцца ў электронным відзе, з\'яўляюцца <em>Portable Document Format/A</em> (<em>PDF/A</em>), <em>Office Open XML</em> (<em>DOCX</em>), двайны фармат з разметкай (<em>DOC</em>), Rich Text Format (<em>RTF</em>), тэкставы файл (<em>TXT</em>), <em>Open Document Format</em> (<em>ODT</em>), фармат архівацыі і скарачэння даных (<em>ZIP, RAR</em>), <em>Portable Network Graphics</em> (<em>PNG</em>), <em>Tagged Image File Format</em> (<em>TIFF</em>), <em>Joint Photograph Experts Group</em> (<em>JPEG</em>), <em>Joint Photograph Group</em> (<em>JPG</em>).</p></div> <div class="form-group"><input type="file" name="upload" id="uploadFile" /></div> <div class="text-center" style="padding:20px"><button type="button" formnovalidate value="SendEmail" id="buttonSendEmail">Адправіць зварот</button></div>';
+        profile.appendChild(div);
+        maskTel();
+        //ajax сробатывает при отправе сообщения, чтобы частично обновлять представление
+        $(document).ready(function () {
+            $('#buttonSendEmail').click(function () {
+                ValidName();
+                ValidMessage();
+                ValidDesignation();
+                ValidAddress();
+                ValidEmail();
+                if (_validName == true && _validMessage == true && _validDesignation == true && _validAddress == true && _validEmail == true) {
+                    var formData = new FormData();
+                    formData.append('File', $('#uploadFile')[0].files[0]); // myFile is the input type="file" control
+                    formData.append('Name', $('#fullName').val());
+                    formData.append('Address', $('#address').val());
+                    formData.append('Email', $('#email').val());
+                    formData.append('Phone', $('#tel').val());
+                    formData.append('Message', $('#message').val());
+                    formData.append('Designation', $('#designation').val());
+                    $.ajax({
+                        url: '/service/sendEmail',
+                        type: 'POST',
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        success: function (result) {
+                            $('#resultSendEmail').html(result);
+                        }
+                    });
+                    $('#exampleModalCenter').modal('show')
+                }
+            });
+        });
+    }
+}
+
+//Метод рисования новых форм для юр.лиц (BE).
+function addInputLegalPersonBE() {
+    if (x < 1) {
+        delInput();
+        div.id = 'input' + ++x;
+        div.innerHTML = '<div class="form-group"><input type="text" class="form-control" name="designation" id="designation" placeholder="Назву і (або) адрас арганізацыі альбо пасаду асобы*" required /></div> <div class="form-group"><input type="text" class="form-control" name="nameLegal" id="nameLegal" placeholder="Поўнае найменне юрыдычнай асобы*" required /></div> <div class="form-group"><input type="text" class="form-control" name="fullName" id="fullName" placeholder="Прозвішча, імя або ініцыялы кіраўніка або асобы, упаўнаважанай ва ўстаноўленым парадку падпісваць звароты*" required /></div> <div class="form-group"><input type="text" class="form-control" name="address" id="address" placeholder="Месцазнаходжанне юрыдычнай асобы*" /></div> <div class="form-group"><input type="email" class="form-control" name="email" id="email" placeholder="Электронная пошта*" /></div>  <div class="form-group"><input type="text" class="form-control" name="tel" id="tel" placeholder="Кантактны тэлефон" /></div> <div class="form-group"><textarea class="form-control" name="message" id="message" placeholder="Зварот*" maxlength="2000"></textarea></div> <div><p>Дапушчальнымі фарматамі дакументаў і (або) звестак, якія прымацоўваюцца ў электронным відзе, з\'яўляюцца <em>Portable Document Format/A</em> (<em>PDF/A</em>), <em>Office Open XML</em> (<em>DOCX</em>), двайны фармат з разметкай (<em>DOC</em>), Rich Text Format (<em>RTF</em>), тэкставы файл (<em>TXT</em>), <em>Open Document Format</em> (<em>ODT</em>), фармат архівацыі і скарачэння даных (<em>ZIP, RAR</em>), <em>Portable Network Graphics</em> (<em>PNG</em>), <em>Tagged Image File Format</em> (<em>TIFF</em>), <em>Joint Photograph Experts Group</em> (<em>JPEG</em>), <em>Joint Photograph Group</em> (<em>JPG</em>).</p></div> <div class="form-group"><input type="file" name="upload" id="uploadFile" /></div> <div class="text-center" style="padding:20px"><button type="button" formnovalidate value="SendEmail" id="buttonSendEmail">Адправіць зварот</button></div>';
         profile.appendChild(div);
         maskTel();
         //ajax сробатывает при отправе сообщения, чтобы частично обновлять представление
